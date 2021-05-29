7692c2f7b5a5442fa40b8b8c1a7c9ba4cf117c63
patrick brodie <patrickxbrodie@hotmail.com>
2021-05-28 21:47:31 -0400

    got it to work, abandoned the original js idea

---
+++ script.js
@@ -0,0 +1,59 @@
+// This is the time displayed in the header.
+var datetime = null,
+    date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#currentDay')
+    update();
+    setInterval(update,1000);
+});
+
+
+var time = moment();
+
+function dayScheduler() {
+    thisHour = time.hours();
+    $(".timeSlot").each(function () {
+        var workHour = parseInt($(this).attr("id"));
+        if (workHour > thisHour) {
+            $(this).addClass("future")
+        }
+        else if (workHour === thisHour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+dayScheduler();
+
+// this is the fuction that saves what was written
+function save() {
+    $(".timeSlot").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    });
+}
+save();
+
+
+
+
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function () {
+
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+
+});
